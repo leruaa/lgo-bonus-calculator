4d3366551e904ae21ede0ed78852c0eef9d734c7
aurel <aurelien.catinon@gmail.com>
2019-06-16 21:14:20 +0200

    Renaming dist to docs to enable github pages

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,10 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'docs')
     },
     devServer: {
-        contentBase: './dist',
+        contentBase: './docs',
         writeToDisk: true
     },
     resolve: {
