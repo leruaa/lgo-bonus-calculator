e2a52dfa8d47f0c96ce885b741e92db4a6c024ca
Aurélien Catinon <aurelien.catinon@gmail.com>
2020-02-16 10:46:42 +0100

    Set the last block to 9490743

--- backend/snapshotNewContract.ts
+++ backend/snapshotNewContract.ts
@@ -17,13 +17,17 @@ import { BigNumber } from 'bignumber.js';
     const apiUrl = args[2];
     const web3 = new Web3(new Web3.providers.HttpProvider(apiUrl));
     const contract = new web3.eth.Contract(newTokenContractAbi, newTokenContractAddress);
-    const latestBlock = await web3.eth.getBlock("latest");
+    let latestBlock = await web3.eth.getBlock("latest");
     let snapshot = require("../config/snapshot.json") as TokenSnapshot;
 
     snapshot.totalUnspentAmount = new BigNumber(snapshot.totalUnspentAmount);
 
     for (let address of Object.keys(snapshot.holders)) {
         snapshot.holders[address] = Holder.instanciate(snapshot.holders[address]);
+    }
+
+    if (latestBlock.number > 9490743) {
+        latestBlock = await web3.eth.getBlock(9490743);
     }
 
     snapshot = await updateSnapshot(snapshot, contract, latestBlock, tokenDecimals);
